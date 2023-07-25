14e9c28faa26bf6e9403426e87aef5ca29b67c1e
sommizzu <someee4399@gmail.com>
2023-07-25 14:49:46 +0000

    NOVICE LOW-01-Changing the value of a variable

---
+++ CodeTree_NOVICE LOW/출력/변수 값 변경/문자 변경.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a = "C"; // std::string으로 문자열 저장
+
+    a = "T"; // std::string을 'T'로 변경
+    cout << a << endl;
+
+    return 0;
+}
+//char은 한문자만을 저장하기에 적합하지 X 
